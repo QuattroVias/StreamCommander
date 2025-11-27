3b7db09cdd4aaac1e64ccab4bfc72640c2cb0a6a
QuattroVias <avtodark@yandex.ru>
2025-11-27 21:25:00 +0300

    Remove big binaries manually

--- tools/over2/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('overlayAPI', {
-  toggleOverlay: (shouldShow) => ipcRenderer.send('toggle-overlay', shouldShow),
-  updateWindowSize: (width, height) => ipcRenderer.send('update-window-size', { width, height }),
-  updateZoomFactor: (zoom) => ipcRenderer.send('update-zoom-factor', zoom)
-});
